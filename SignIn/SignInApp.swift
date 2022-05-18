1c7dd216fc8070be1aed36327190c2cbad7a404d
meszkos <michalik515@gmail.com>
2022-05-18 11:10:07 +0200

    Initial Commit

---
+++ SignIn/SignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInApp.swift
+//  SignIn
+//
+//  Created by MacOS on 18/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
