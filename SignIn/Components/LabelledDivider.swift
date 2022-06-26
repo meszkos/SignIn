f54a3f1d67e2276966e8834c26bebec47fe8c166
meszkos <michalik515@gmail.com>
2022-06-26 13:16:43 +0200

    Uber SignIn view

---
+++ SignIn/Components/LabelledDivider.swift
@@ -0,0 +1,34 @@
+//
+//  LabelledDivider.swift
+//  SignIn
+//
+//  Created by MacOS on 11/06/2022.
+//
+
+import SwiftUI
+
+
+struct LabelledDivider: View {
+
+    let label: String
+    let horizontalPadding: CGFloat
+    let color: Color
+
+    init(label: String, horizontalPadding: CGFloat = 20, color: Color = .gray) {
+        self.label = label
+        self.horizontalPadding = horizontalPadding
+        self.color = color
+    }
+
+    var body: some View {
+        HStack {
+            line
+            Text(label).foregroundColor(color)
+            line
+        }
+    }
+
+    var line: some View {
+        VStack { Divider().background(color) }.padding(horizontalPadding)
+    }
+}
---
+++ SignIn/Views/UberSignInView.swift
@@ -0,0 +1,115 @@
+//
+//  UberSignInView.swift
+//  SignIn
+//
+//  Created by MacOS on 26/06/2022.
+//
+
+import SwiftUI
+
+struct UberSignInView: View {
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        ZStack{
+            Image("NYC")
+                .resizable()
+            Color.black
+                .opacity(0.5)
+            VStack{
+                uberLogo
+                uber
+                
+                GreyTextField(imageName: "person",
+                              imageColor: .black,
+                              placeholderText: "Email",
+                              isSecureField: false,
+                              text: $email)
+                GreyTextField(imageName: "lock",
+                              imageColor: .black,
+                              placeholderText: "Password",
+                              isSecureField: true,
+                              text: $password)
+                signInButton
+                forgotPasswordText
+                socialStack
+
+            }
+            .padding()
+        }
+        .ignoresSafeArea(edges: [.top, .bottom])
+    }
+}
+
+struct UberSignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        UberSignInView()
+    }
+}
+
+extension UberSignInView{
+    
+//MARK: - Header
+    
+    var uberLogo: some View{
+        Image("uber")
+            .resizable()
+            .frame(width: 100, height: 100)
+            .foregroundColor(.white)
+    }
+    var uber: some View{
+        Text("U B E R")
+            .foregroundColor(.white)
+            .bold()
+            .font(.system(size: 70))
+            .offset(y: -30)
+            .padding(.bottom, 40)
+    }
+
+//MARK: - Buttons
+    
+    var signInButton: some View{
+        Button {
+            //Log in screen
+        } label: {
+            Text("Sign In")
+                .foregroundColor(.white)
+                .frame(height: 40)
+                .frame(maxWidth: .infinity)
+                .background(Color.gray)
+                .cornerRadius(8)
+                .padding(.top, 20)
+        }
+    }
+    var forgotPasswordText: some View{
+        NavigationLink {
+            //Forgot password
+        } label: {
+            Text("Forgot password?")
+                .foregroundColor(.white)
+        }
+    }
+    
+    var socialStack: some View{
+        HStack(spacing: 20){
+            Button {
+                //Sign in with FB
+            } label: {
+                Image("fbIcon")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+            }
+            Button {
+                //Sign in with google
+            } label: {
+                Image("googleIcon")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+            }
+        }
+        .padding(.vertical, 30)
+    }
+    
+}
